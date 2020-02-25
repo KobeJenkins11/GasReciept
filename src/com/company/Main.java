5e7e9519665779eaa00d91b061f159c8872f4199
kobejenkins11 <kobe.jenkins.2022@student.lhschools.org>
2020-02-25 12:04:11 -0500

    Intelij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code
+        System.out.println(" ____________________________________");
+        System.out.println("|              CarFood               |");
+        System.out.println("|                                    |");
+        System.out.println("|343 Rosewood Drive                  |");
+        System.out.println("| Columbus OH 43004                  |");
+        System.out.println("|                                    |");
+        System.out.println("|DATE 1/16/2020     TIME 12:22 PM    |");
+        System.out.println("|INVOICE#        21212121            |");
+        System.out.println("|AUTH#                               |");
+        System.out.println("|             MASTERCARD             |");
+        System.out.println("|           ACCOUNT  NUMBER          |");
+        System.out.println("|          ************17            |");
+        System.out.println("|pump         product       $/G      |");
+        System.out.println("|08             UNLD         $3.0    |");
+        System.out.println("| GALLONS     FUEL     TOTAL         |");
+        System.out.println("| 25.50                 $60.00       |");
+        System.out.println("|                                    |");
+        System.out.println("|                                    |");
+        System.out.println("|          THANK YOU!                |");
+        System.out.println("|          CARFOOD                   |");
+        System.out.println("|____________________________________|");
+    }
+
+}
